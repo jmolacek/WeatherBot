d0a9f0854b3ce4b60b016c43aba0f9d725d0de3c
Molacek, Jordan <Jordan.Molacek@gmail.com>
2017-06-06 13:43:04 -0500

    no message

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.all('/weather', function(request, response) {
 
         response.send({
             "color": "green",
-            "message": sendString + '\n' + JSON.stringify(request.body),
+            "message": sendString + '\n' + JSON.stringify(request.body.item.message.from.name),
             "notify": false,
             "message_format": "text"
         });
